ff4b126bdfe6b81124a7be44e65257403c99d7f3
javierf39 <javi.freire@duocuc.cl>
2022-11-22 14:05:39 -0400

    listo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
+const enviarCorreo = require('../funciones/enviarCorreo');
 
 const router = Router();
 
@@ -10,34 +11,15 @@ router.get('/', (req, res) => {
 router.post('/email', (req, res) => {
 
     const { name, email, mensaje } = req.body;
-    contentHTML = `
-        <h1>${name} quiere contratar nuestros servicios</h1>
-        <p>Sus datos de contaccto son ${email}</p>
-        <p>${mensaje}</p>
-    `;
+    const correoAdmin = 'prueba@javierfreire.cl';
 
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.titan.email',
-        port: 465,
-        secure: true,
-        auth: {
-            user: 'prueba@javierfreire.cl',
-            pass: 'jkulmnsit29taza'
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
+    //correoAdmin
+    enviarCorreo(0, name, correoAdmin, email, mensaje);
 
-    const info = transporter.sendMail({
-        from: "'Correo de prueba' <prueba@javierfreire.cl>",
-        to: 'javierfreire39@gmail.com',
-        subject: 'Pruebas',
-        html: contentHTML
-    });
+    //correoCliente
+    enviarCorreo(1, name, email, email, mensaje);
 
-    console.log('mensaje ', info.messageId);
-    console.log(req.body)
+    console.log(req.body);
 
     res.json({ mensaje: 'enviado' });
 });
