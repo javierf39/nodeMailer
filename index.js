95901b6b2c21f7052b8d2e27f6e4f2ca96f5c4df
javierf39 <javi.freire@duocuc.cl>
2022-11-24 22:22:20 -0400

    diseño correo

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(__dirname + 'public'));
 app.use(require('./src/routes/index'));
 
 app.listen(port, () => {
